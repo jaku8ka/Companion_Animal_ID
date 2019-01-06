1739460bb6899f3d8e2d2279a43417637cb8274e
jaku8ka <jakub.blahunka@gmail.com>
2019-01-06 21:47:27 +0100

    name of main menu

--- app/src/main/java/org/jaku8ka/companionAnimal/MainActivity.java
+++ app/src/main/java/org/jaku8ka/companionAnimal/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ItemCli
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(R.string.app_main);
         setContentView(R.layout.activity_main);
 
 
